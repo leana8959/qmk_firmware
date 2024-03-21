f1b87b8ae83b2311fdc7cf53e40a6ae60e5b73a0
Léana 江 <leana.jiang@icloud.com>
2024-03-21 21:55:10 +0100

    fixes

--- keyboards/zsa/moonlander/keymaps/leana8959/keymap.c
+++ keyboards/zsa/moonlander/keymaps/leana8959/keymap.c
@@ -23,6 +23,7 @@
 
 #include QMK_KEYBOARD_H
 #include "version.h"
+#include "modifiers.h"
 #include "keymap_dvorak.h"
 
 enum layers {
@@ -44,24 +45,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         KC_CAPS,  DV_QUOT, DV_COMM, DV_DOT,  DV_P,    DV_Y,    KC_BRID,          KC_VOLD,   DV_F,    DV_G,    DV_C,    DV_R,    DV_L,     DV_SLSH,
         LT(MUS, KC_ESC),   DV_A,    DV_O,    DV_E,    DV_U,    DV_I,    DV_PLUS,          DV_EQL,    DV_D,    DV_H,    DV_T,    DV_N,    DV_S,     DV_MINS,
-        MO(SYMB), DV_Z,    DV_Q,    DV_J,    DV_K,    DV_X,                                 DV_B,    DV_M,    DV_W,    DV_V,    DV_Z,     MO(SYMB),
+        MO(SYMB), DV_SCLN, DV_Q,    DV_J,    DV_K,    DV_X,                                 DV_B,    DV_M,    DV_W,    DV_V,    DV_Z,     MO(SYMB),
 
         // TODO: macro to open iTerm on red button on the right hand
         // TODO: macro to screenshot on red button on the left hand
-        KC_APP,   MO(SYMB),KC_LCTL, MT(KC_LOPT, KC_UP), MT(KC_LCMD, KC_DOWN),         _______,           _______,            MT(KC_RCMD, KC_LEFT), MT(KC_ROPT, KC_RIGHT), KC_RCTL, MO(SYMB), _______,
+        KC_APP,   MO(SYMB), KC_LCTL, MT(MOD_LALT, KC_DOWN), MT(MOD_LGUI, KC_UP),         _______,           _______,            MT(MOD_RGUI, KC_LEFT), MT(MOD_RALT, KC_RIGHT), KC_RCTL, MO(SYMB), _______,
                                              KC_SPC,  KC_TAB, DV_DLR,            DV_GRV,    KC_ENT,  KC_BSPC
     ),
 
 
     [SYMB] = LAYOUT(
         // Hijack shift behaviour for the number row
-        _______,  DV_EXLM,    DV_LBRC,    DV_LPRN,    DV_LBRC,    DV_PIPE,    _______,          _______,   DV_AMPR,    DV_RBRC,    DV_RPRN,    DV_RBRC,    TG(FN),     _______,
+        _______,  DV_EXLM,    DV_LBRC,    DV_LPRN,    DV_LBRC,    DV_PERC,    _______,          _______,   DV_CIRC,    DV_RBRC,    DV_RPRN,    DV_RBRC,    TG(FN),     _______,
         // Shift alphabets normally
-        _______,  S(DV_QUOT), S(DV_COMM), S(DV_DOT),  S(DV_P),    S(DV_Y),    _______,          _______,   S(DV_F),    S(DV_G),    S(DV_C),    S(DV_R),    S(DV_L),     DV_QUES,
+        _______,  S(DV_QUOT), S(DV_COMM), S(DV_DOT),  S(DV_P),    S(DV_Y),    DV_PIPE,          DV_AMPR,   S(DV_F),    S(DV_G),    S(DV_C),    S(DV_R),    S(DV_L),     DV_QUES,
         _______,  S(DV_A),    S(DV_O),    S(DV_E),    S(DV_U),    S(DV_I),    DV_ASTR,          DV_HASH,   S(DV_D),    S(DV_H),    S(DV_T),    S(DV_N),    S(DV_S),     DV_UNDS,
-        _______,  S(DV_Z),    S(DV_Q),    S(DV_J),    S(DV_K),    S(DV_X),                                 S(DV_B),    S(DV_M),    S(DV_W),    S(DV_V),    S(DV_Z),     _______,
-        _______,  _______,    _______,    _______,    _______,                _______,          _______,               _______,    _______,    _______,    _______,     _______,
-                                                      _______,    _______,    DV_AT,            DV_TILD,   _______,    _______
+        _______,  DV_COLN,    S(DV_Q),    S(DV_J),    S(DV_K),    S(DV_X),                                 S(DV_B),    S(DV_M),    S(DV_W),    S(DV_V),    S(DV_Z),     _______,
+        S(KC_APP),_______,    _______,    _______,    _______,                _______,          _______,               _______,    _______,    _______,    _______,     _______,
+                                                      _______,    S(KC_TAB),  DV_AT,            DV_TILD,   S(KC_ENT),  _______
     ),
 
     [FN] = LAYOUT(
