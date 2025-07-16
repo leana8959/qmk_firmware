a24945d8fbc24bc9ac4f130e49121e1fae52e2fb
Léana 江 <leana.jiang+git@icloud.com>
2025-07-16 18:25:20 +0200

    make native layout follow up fake map

--- keyboards/zsa/moonlander/keymaps/leana8959/keymap.c
+++ keyboards/zsa/moonlander/keymaps/leana8959/keymap.c
@@ -56,14 +56,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [L_NATV] = LAYOUT(
         _______,  _______, _______, _______, _______, _______, _______,          KC_LBRC,  _______, _______, _______, _______, _______, _______,
-        _______,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    _______,          KC_RBRC,  KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
-        KC_ESC,   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_PLUS,          KC_EQL,   KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+        KC_GRV,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    _______,          KC_RBRC,  KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+        KC_ESC,   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_BSLS,          KC_EQL,   KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
         // ^ assume that we don't have os mapping for this layer
         _______,  MT(MOD_LCTL,KC_SCLN),
                            KC_Q,    KC_J,    KC_K,    KC_X,                                KC_B,    KC_M,    KC_W,    KC_V,    MT(MOD_RCTL,KC_Z),
                                                                                                                                         _______,
         _______,  _______, _______, _______, _______,          _______,          _______,           _______, _______, _______, _______, DF(L_BASE),
-                                             _______, _______, KC_DLR,            KC_GRV,  _______, _______
+                                             _______, _______, KC_NO,            KC_GRV,   _______, _______
     ),
 
     [L_FUNC] = LAYOUT(
