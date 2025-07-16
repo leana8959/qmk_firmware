cd94da5e3f601469694982064d55bba694f2c912
Léana 江 <leana.jiang+git@icloud.com>
2025-07-16 14:07:30 +0200

    map esc on native layer

--- keyboards/zsa/moonlander/keymaps/leana8959/keymap.c
+++ keyboards/zsa/moonlander/keymaps/leana8959/keymap.c
@@ -52,7 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_NATV] = LAYOUT(
         _______,  _______, _______, _______, _______, _______, _______,          _______,  _______, _______, _______, _______, _______, _______,
         _______,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    _______,          _______,  KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
-        _______,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_PLUS,          KC_EQL,   KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+        KC_ESC,   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_PLUS,          KC_EQL,   KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+        // ^ assume that we don't have os mapping for this layer
         _______,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                                KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    _______,
         _______,  _______, _______, _______, _______,          _______,          _______,           _______, _______, _______, _______, DF(L_BASE),
                                              _______, _______, KC_DLR,            KC_GRV,  _______, _______
