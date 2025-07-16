b8425de842ce5ecbf039334a699fd33685639a2a
Léana 江 <leana.jiang+git@icloud.com>
2025-07-16 18:34:13 +0200

    remove credits

--- keyboards/zsa/moonlander/keymaps/leana8959/keymap.c
+++ keyboards/zsa/moonlander/keymaps/leana8959/keymap.c
@@ -16,11 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Inspiration:
- * https://github.com/ThePrimeagen/keyboards
- */
-
 #include QMK_KEYBOARD_H
 #include "keymap_dvorak.h"
 
