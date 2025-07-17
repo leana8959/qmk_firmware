219255f0046627cccf9e3f8556b9f1299d2ced7d
Léana 江 <leana.jiang+git@icloud.com>
2025-07-17 10:12:25 +0200

    debug keycode

--- keyboards/zsa/moonlander/keymaps/leana8959/keymap.c
+++ keyboards/zsa/moonlander/keymaps/leana8959/keymap.c
@@ -212,3 +212,14 @@ void keyboard_post_init_user(void)
 {
   rgb_matrix_mode_noeeprom(RGB_MATRIX_TYPING_HEATMAP);
 }
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+#ifdef CONSOLE_ENABLE
+  if (debug_enable)
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n",
+            keycode, record->event.key.col, record->event.key.row, record->event.pressed,
+            record->event.time, record->tap.interrupted, record->tap.count);
+#endif
+  return true;
+}
